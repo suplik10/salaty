fa0ef47454ca9cc170a1718019ca14e1093667fb
viktorkafka <viktor.kafka1@albixon.cz>
2018-01-29 06:16:17 +0100

    newsletter

--- htdocs/app/presenters/Error4xxPresenter.php
+++ htdocs/app/presenters/Error4xxPresenter.php
@@ -2,12 +2,27 @@
 
 namespace App\Presenters;
 
+use App\Forms\UserFormFactory;
+use App\Model\UserModel;
 use Nette;
 
 
 class Error4xxPresenter extends Nette\Application\UI\Presenter
 {
-	public function startup()
+    /**
+     * @var UserFormFactory
+     * @inject
+     */
+    public $userForm;
+
+    /**
+     * @var UserModel
+     * @inject
+     */
+    public $userModel;
+
+
+    public function startup()
 	{
 		parent::startup();
 		if (!$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
@@ -22,4 +37,27 @@ class Error4xxPresenter extends Nette\Application\UI\Presenter
 		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
 		$this->template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
 	}
+
+
+    protected function createComponentSignForNewsletter()
+    {
+        $form = $this->userForm->signForNewsletter();
+        $form->onSuccess[] = [$this, 'signForNewsletterSucceeded'];
+        return $form;
+    }
+
+    public function signForNewsletterSucceeded(Nette\Application\UI\Form $form)
+    {
+        $values = $form->getValues();
+        try {
+            $isSignedUp = $this->userModel->isEmailSignedForNewsletter($values->email);
+            if ($isSignedUp) {
+                throw new \Exception('Tento e-mail se již nachází v databázi pro odběr novinek');
+            }
+            $this->userModel->signForNewsletter($values->email);
+            $this->flashMessage('Přihlášení k odběru proběhlo úspěšně.', 'success');
+        } catch (\Exception $e) {
+            $this->flashMessage($e->getMessage(), 'danger');
+        }
+    }
 }
